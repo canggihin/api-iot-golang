abae97fd804702487fa97e25e89ab2a912b0b61e
mchwalfajar <mchwalfajar@gmail.com>
2024-06-12 16:41:02 +0700

    update data sensor by day

--- models/models.go
+++ models/models.go
@@ -9,10 +9,10 @@ type SensorData struct {
 	FormattedTime string  `json:"formattedTime"`
 }
 
-type SensorDataResponse struct {
-	Temperature string `json:"temperature"`
-	Humidity    string `json:"humidity"`
-	Message     string `json:"message"`
-	RainWasFall string `json:"rain_was_fall"`
-	Pressure    string `json:"pressure"`
+type SensorDataByDay struct {
+	Temperature   float64 `json:"temperature"`
+	Humidity      float64 `json:"humidity"`
+	RainWasFall   float64 `json:"rain_was_fall"`
+	Pressure      float64 `json:"pressure"`
+	FormattedTime string  `json:"formattedTime"`
 }
