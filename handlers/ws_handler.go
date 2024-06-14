28a39d910d0fd107bbb952db690c0088af21f549
mchwalfajar <mchwalfajar@gmail.com>
2024-06-15 06:29:34 +0700

    update timer

--- handlers/ws_handler.go
+++ handlers/ws_handler.go
@@ -12,29 +12,50 @@ import (
 )
 
 func handleMessages(client *pkg.Client) {
+	defer func() {
+		if client.Timer != nil {
+			client.Timer.Stop()
+		}
+	}()
+
 	for {
 		_, msg, err := client.Conn.ReadMessage()
 		if err != nil {
 			fmt.Printf("%s Data read error: %v\n", client.Type, err)
-			break
+			return // Exit the loop and let defer clean up
 		}
 		fmt.Printf("Received %s Data: %s\n", client.Type, string(msg))
 
-		// Ensure timer is actively managed and reset here.
+		// Reset the timer on message receipt
 		if client.Timer != nil {
 			client.Timer.Reset(10 * time.Second)
-		} else {
-			fmt.Println("Timer is nil, can't reset")
 		}
 	}
 }
 
-func (h *handlers) HandleWsSensor(c *gin.Context) {
-	handleCWs(c, "sensor")
+func setupTimer(client *pkg.Client) {
+	client.Timer = time.AfterFunc(10*time.Second, func() {
+		sendStatus(client)
+		client.Timer.Reset(10 * time.Second) // Immediately reset the timer
+	})
 }
 
-func (h *handlers) HandleWsSystem(c *gin.Context) {
-	handleCWs(c, "system")
+func sendStatus(client *pkg.Client) {
+	var data models.SystemInfo
+	data.Status = 0
+	data.TotalSensor = 0
+	data.RamConsume = ""
+	data.CpuConsume = ""
+	data.DHTSensor = 0
+	data.BMP180Sensor = 0
+	data.RainSensor = 0
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		fmt.Println("Error marshalling data: ", err)
+		return
+	}
+	fmt.Println("WebSocket inactive for 10 seconds, sending status 0")
+	client.Conn.WriteMessage(websocket.TextMessage, jsonData)
 }
 
 func handleCWs(c *gin.Context, clientType string) {
@@ -45,37 +66,19 @@ func handleCWs(c *gin.Context, clientType string) {
 	}
 
 	client := &pkg.Client{Conn: conn, Type: clientType}
-
 	setupTimer(client)
-
-	handleMessages(client)
 
 	pkg.AddClient(client)
 	defer pkg.RemoveClient(client)
 	defer client.Conn.Close()
-	defer func() {
-		if client.Timer != nil {
-			client.Timer.Stop()
-		}
-	}()
+
+	handleMessages(client)
 }
 
-func setupTimer(client *pkg.Client) {
-	client.Timer = time.AfterFunc(10*time.Second, func() {
-		var data models.SystemInfo
-		data.Status = 0
-		data.TotalSensor = 0
-		data.RamConsume = ""
-		data.CpuConsume = ""
-		data.DHTSensor = 0
-		data.BMP180Sensor = 0
-		data.RainSensor = 0
-		jsonData, err := json.Marshal(data)
-		if err != nil {
-			fmt.Println("Error marshalling data: ", err)
-			return
-		}
-		fmt.Println("WebSocket inactive for 10 seconds, sending status 0")
-		client.Conn.WriteMessage(websocket.TextMessage, jsonData)
-	})
+func (h *handlers) HandleWsSensor(c *gin.Context) {
+	handleCWs(c, "sensor")
+}
+
+func (h *handlers) HandleWsSystem(c *gin.Context) {
+	handleCWs(c, "system")
 }
